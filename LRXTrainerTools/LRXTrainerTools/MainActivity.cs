8bb6eb459bf5ca8b43f13c205de3e767cb1f434b
Colten Jernigan <caj20@students.uwf.edu>
2015-11-14 02:28:48 -0600

    GUI
    Messed with the GUI a bit.

--- LRXTrainerTools/LRXTrainerTools/MainActivity.cs
+++ LRXTrainerTools/LRXTrainerTools/MainActivity.cs
@@ -9,25 +9,35 @@ using Android.OS;
 
 namespace LRXTrainerTools
 {
-	[Activity (Label = "LRXTrainerTools", MainLauncher = true, Icon = "@drawable/icon")]
+	[Activity (Label = "LRX Trainer Tools", MainLauncher = true, Icon = "@drawable/icon")]
 	public class MainActivity : Activity
 	{
 		int count = 1;
+		string tab1_text = "Metronome";
 
 		protected override void OnCreate (Bundle bundle)
 		{
 			base.OnCreate (bundle);
 
 			// Set our view from the "main" layout resource
-			SetContentView (Resource.Layout.Main);
+			ActionBar.NavigationMode = ActionBarNavigationMode.Tabs;
+			SetContentView (Resource.Layout.Main); 
+
+			ActionBar.Tab metronomeTab = ActionBar.NewTab();
+			metronomeTab.SetText(Resources.GetString(Resource.String.tab1_text));
+			metronomeTab.SetIcon(Resource.Drawable.tab1_icon);
+			//metronomeTab.TabSelected += (sender, args) => {
+				// Do something when tab is selected
+			//}
+				ActionBar.AddTab(metronomeTab);
 
 			// Get our button from the layout resource,
 			// and attach an event to it
-			Button button = FindViewById<Button> (Resource.Id.myButton);
+			//Button button = FindViewById<Button> (Resource.Id.myButton);
 			
-			button.Click += delegate {
-				button.Text = string.Format ("{0} clicks!", count++);
-			};
+			//button.Click += delegate {
+			//	button.Text = string.Format ("{0} clicks!", count++);
+			//};
 		}
 	}
 }
