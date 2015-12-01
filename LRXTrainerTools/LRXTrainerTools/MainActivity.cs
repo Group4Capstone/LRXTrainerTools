598953307f4704419f199d3c7b56fd9e28cad9fa
Colten Jernigan <caj20@students.uwf.edu>
2015-12-01 05:34:08 -0600

    Some progress made...
    Not much though.

--- LRXTrainerTools/LRXTrainerTools/MainActivity.cs
+++ LRXTrainerTools/LRXTrainerTools/MainActivity.cs
@@ -15,13 +15,123 @@ namespace LRXTrainerTools
 
 		protected override void OnCreate (Bundle bundle)
 		{
+
+			base.OnCreate (bundle);
+
+			// Set our view from the "main" layout resource
+			SetContentView (Resource.Layout.actionBar);
+
+			this.ActionBar.NavigationMode = ActionBarNavigationMode.Tabs;
+
+			AddTab ("Metronome", Resource.Drawable.metronomeIcon, new SampleTabFragment ());
+			AddTab ("Stopwatch", Resource.Drawable.stopwatchIcon, new SampleTabFragment2 ());
+
+			if (bundle != null)
+				this.ActionBar.SelectTab(this.ActionBar.GetTabAt(bundle.GetInt("tab")));
+
+		}
+
+		protected override void OnSaveInstanceState(Bundle outState)
+		{
+			outState.PutInt("tab", this.ActionBar.SelectedNavigationIndex);
+
+			base.OnSaveInstanceState(outState);
+		}
+
+		void AddTab (string tabText, int iconResourceId, Fragment view)
+		{
+			var tab = this.ActionBar.NewTab ();            
+			tab.SetText (tabText);
+			tab.SetIcon (Resource.Drawable.metronomeIcon);
+
+			// must set event handler before adding tab
+			tab.TabSelected += delegate(object sender, ActionBar.TabEventArgs e)
+			{
+				var fragment = this.FragmentManager.FindFragmentById(Resource.Id.fragmentContainer);
+				if (fragment != null)
+					e.FragmentTransaction.Remove(fragment);         
+				e.FragmentTransaction.Add (Resource.Id.fragmentContainer, view);
+			};
+			tab.TabUnselected += delegate(object sender, ActionBar.TabEventArgs e) {
+				e.FragmentTransaction.Remove(view);
+			};
+
+			this.ActionBar.AddTab (tab);
+		}
+
+		class SampleTabFragment: Fragment
+		{            
+			public override View OnCreateView (LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+			{
+				base.OnCreateView (inflater, container, savedInstanceState);
+
+				var view = inflater.Inflate (Resource.Layout.metronomeView, container, false);
+
+				return view;
+			}
+		}
+
+		class SampleTabFragment2 : Fragment
+		{
+			public override View OnCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+			{
+				base.OnCreateView(inflater, container, savedInstanceState);
+
+				var view = inflater.Inflate(Resource.Layout.stopwatchView, container, false);
+
+				return view;
+			}
+		}
+
+		/*
+			//ActionBar.NavigationMode = ActionBarNavigationMode.Tabs;
+			base.OnCreate (bundle);
+			SetContentView(Resource.Layout.actionBar);
+
+			var trans = FragmentManager.BeginTransaction ();
+			trans.Add (Resource.Id.fragmentContainer, new metronomeFragment (), "metronomeFragment");
+			trans.Commit ();
+
+			var trans2 = FragmentManager.BeginTransaction ();
+			trans2.Add (Resource.Id.fragmentContainer, new stopwatchFragment (), "stopwatchFragment");
+			trans2.Commit ();
+		*/
+			//ActionBar.Tab tab = ActionBar.NewTab();
+			//tab.SetText(Resources.GetString(Resource.String.metTabText));
+			//tab.SetIcon(Resource.Drawable.metronomeIcon);
+			//tab.TabSelected += (sender, args) => {
+				// Do something when tab is selected
+			//};
+			//	ActionBar.AddTab(tab);
+
+			//tab = ActionBar.NewTab();
+			//tab.SetText(Resources.GetString(Resource.String.stopwatchTabText));
+			//tab.SetIcon(Resource.Drawable.stopwatchIcon);
+			//tab.TabSelected += (sender, args) => {
+				// Do something when tab is selected
+			//};
+			//	ActionBar.AddTab(tab);
+
+			/*ActionBar.NavigationMode = ActionBarNavigationMode.Tabs;
+
 			base.OnCreate (bundle);
 
 			ActionBar.SetCustomView (Resource.Layout.actionBar);
 			ActionBar.SetDisplayShowCustomEnabled (true);
 
-			//Set our view from the "main" layout resource
-			//SetContentView(Resource.Layout.Main);
-		}
+			ActionBar.Tab metTab = ActionBar.NewTab ();
+			metTab.TabSelected += (sender, e) => 
+			{
+				//gkg
+			};
+			ActionBar.AddTab(metTab);
+
+			ActionBar.Tab watchTab = ActionBar.NewTab ();
+			watchTab.TabSelected += (sender, e) => 
+			{
+				//Do something
+			};
+			ActionBar.AddTab (watchTab);
+		*/
 	}
 }
--- LRXTrainerTools/LRXTrainerTools/Resources/Resource.designer.cs
+++ LRXTrainerTools/LRXTrainerTools/Resources/Resource.designer.cs
@@ -73,22 +73,82 @@ namespace LRXTrainerTools
 		{
 			
 			// aapt resource value: 0x7f060001
-			public const int imageView1 = 2131099649;
+			public const int actionBar = 2131099649;
 			
-			// aapt resource value: 0x7f060004
-			public const int imageView2 = 2131099652;
+			// aapt resource value: 0x7f060010
+			public const int button1 = 2131099664;
+			
+			// aapt resource value: 0x7f060019
+			public const int clearButton = 2131099673;
+			
+			// aapt resource value: 0x7f060008
+			public const int fragmentContainer = 2131099656;
 			
 			// aapt resource value: 0x7f060000
-			public const int linearLayout1 = 2131099648;
+			public const int fullScreen = 2131099648;
 			
 			// aapt resource value: 0x7f060003
-			public const int linearLayout2 = 2131099651;
+			public const int imageView1 = 2131099651;
+			
+			// aapt resource value: 0x7f060006
+			public const int imageView2 = 2131099654;
+			
+			// aapt resource value: 0x7f060012
+			public const int linearLayout = 2131099666;
+			
+			// aapt resource value: 0x7f060004
+			public const int metText = 2131099652;
 			
 			// aapt resource value: 0x7f060002
-			public const int metText = 2131099650;
+			public const int metronomeButton = 2131099650;
+			
+			// aapt resource value: 0x7f060011
+			public const int parentFrameLayout = 2131099665;
+			
+			// aapt resource value: 0x7f06000c
+			public const int pitchBar = 2131099660;
+			
+			// aapt resource value: 0x7f06000b
+			public const int pitchText = 2131099659;
+			
+			// aapt resource value: 0x7f06000f
+			public const int relativeLayout1 = 2131099663;
+			
+			// aapt resource value: 0x7f060015
+			public const int scrollView = 2131099669;
+			
+			// aapt resource value: 0x7f060018
+			public const int startButton = 2131099672;
+			
+			// aapt resource value: 0x7f060017
+			public const int stopButton = 2131099671;
+			
+			// aapt resource value: 0x7f060007
+			public const int stopWatchText = 2131099655;
 			
 			// aapt resource value: 0x7f060005
-			public const int stopWatchText = 2131099653;
+			public const int stopwatchButton = 2131099653;
+			
+			// aapt resource value: 0x7f06000e
+			public const int tempoBar = 2131099662;
+			
+			// aapt resource value: 0x7f06000d
+			public const int tempoText = 2131099661;
+			
+			// aapt resource value: 0x7f060016
+			public const int timeLogText = 2131099670;
+			
+			// aapt resource value: 0x7f060014
+			public const int timer = 2131099668;
+			
+			// aapt resource value: 0x7f060013
+			public const int timerBackground = 2131099667;
+			
+			// aapt resource value: 0x7f060009
+			public const int volText = 2131099657;
+			
+			// aapt resource value: 0x7f06000a
+			public const int volumeBar = 2131099658;
 			
 			static Id()
 			{
@@ -106,6 +166,12 @@ namespace LRXTrainerTools
 			// aapt resource value: 0x7f030000
 			public const int actionBar = 2130903040;
 			
+			// aapt resource value: 0x7f030001
+			public const int metronomeView = 2130903041;
+			
+			// aapt resource value: 0x7f030002
+			public const int stopwatchView = 2130903042;
+			
 			static Layout()
 			{
 				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
@@ -120,10 +186,13 @@ namespace LRXTrainerTools
 		{
 			
 			// aapt resource value: 0x7f040000
-			public const int Hello = 2130968576;
+			public const int app_name = 2130968576;
 			
 			// aapt resource value: 0x7f040001
-			public const int app_name = 2130968577;
+			public const int metTabText = 2130968577;
+			
+			// aapt resource value: 0x7f040002
+			public const int stopwatchTabText = 2130968578;
 			
 			static String()
 			{
